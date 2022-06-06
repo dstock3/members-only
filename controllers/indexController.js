3998b1295548a65e3ca33f505463a2f7965d8ee3
David Stockdale <dstock3@gmail.com>
2022-06-06 15:41:10 -0500

    Added index controller.

---
+++ controllers/indexController.js
@@ -0,0 +1,21 @@
+const Message = require('../models/messages')
+
+exports.index = async(req, res, next) => {
+  if (res.locals.currentUser) { res.redirect('/member') }
+  
+  try { let messages = await Message.find().sort([['timeStamp', 'descending']])
+    .populate([{
+      path: 'user',
+      model: 'User'
+    }, {
+      path: 'replies',
+      model: 'Replies',
+      populate: {
+        path: 'user',
+        model: 'User'
+      }
+    }]);
+
+    res.render('index', { user: req.user, messages: messages }) }
+    catch(err) { return next(err) }
+};
